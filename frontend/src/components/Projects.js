7a47055204ef9818f395813696457684940527e8
SerebroDmitry <q2791@mail.ru>
2023-03-06 17:22:14 +0500

    added googleshhetsData in Projects

--- frontend/src/components/Projects.js
+++ frontend/src/components/Projects.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import '../css/Projects.css'
 
 const Projects = () => {
+  const [tableData, setTableData] = React.useState()
+
+  useEffect(() => {
+    fetch('https://script.google.com/macros/s/AKfycbzuKuSOUfd9IOsQobix0PnIRv28IB3uTx5KdpVoVeNe04g7QzXQeeCEbsHBxE5CYdEV8A/exec') //получение информации из API гугла
+        .then((response) => response.json()) //изменения формата в JSON
+        .then((response) => {
+          //запись значения JSON в переменную tableData
+          setTableData(response['smr'])    
+        }) 
+  }, []);     
+
   return (
-    <div>Projects</div>
+    <div>
+    {tableData && tableData.map(item => (
+      <div key={item.id} className='mainTableDiv'>
+        <div className='LeftTableDiv'>
+          <p className='Adress'>{item.adress}</p>
+        </div>
+        <div className='RightTableDiv'>
+          <span className='ErpGfcName'>ЕРП: </span>
+          <span className='Information'>{item.ERP}</span>
+          <span className='ErpGfcName'>,    ГФК: </span>
+          <span className='Information'>{item.GFK}</span><br />
+          <span className='ErpGfcName'>Наименование: </span>
+          <span className='Information'>{item.object_name}</span>
+        </div>
+        
+      </div>
+    ))}
+    </div>
   )
+  
 }
 
 export default Projects
